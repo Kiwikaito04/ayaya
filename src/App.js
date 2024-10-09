9a9b40ad4f6f2bc2898c0b17a37158ef8f4ea517
karuto1311 <hhh131120044@gmail.com>
2024-10-09 20:01:57 +0700

    cap nhat tam thoi slideshow cho trang chu (anh cua slideshow can phai cap nhat them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Home from './Home';
 import Schedule from './Schedule';
 
