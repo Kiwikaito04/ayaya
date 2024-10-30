8fd6dab36f4bf5b7e0f01ece5fdaa581b888e044
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-10-30 10:53:57 +0700

    last hotfix for "shy phá vl"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,52 +5,20 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Home from './Home';
 import Schedule from './Schedule';
-<<<<<<< HEAD
 import Login from './Components/Login';
 import Signup from './Components/Signup'
 
 function App() {
   const [count, setCount] = useState(0);
 
-=======
-<<<<<<< HEAD:frontend/src/App.js
-import Login from './Components/Login';
-import Signup from './Components/Signup'
-=======
-<<<<<<< HEAD
-import Login from './Components/Login';
-import Signup from './Components/Signup'
-=======
-import Server from './Server'
->>>>>>> 5e966189d8b5afd5356dd5d6e0b558344b39de4e
->>>>>>> cd398f4b871236b8ed2dae437450e2e3639b3cc2:src/App.js
-
-function App() {
-  const [count, setCount] = useState(0);
-  
->>>>>>> 02048e9fea1de1757b955528ebbe2ada3e634222
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/schedule" element={<Schedule />} />
-<<<<<<< HEAD
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
-=======
-<<<<<<< HEAD:frontend/src/App.js
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-=======
-<<<<<<< HEAD
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-=======
-          <Route path="/server" element={<Server />} />
->>>>>>> 5e966189d8b5afd5356dd5d6e0b558344b39de4e
->>>>>>> cd398f4b871236b8ed2dae437450e2e3639b3cc2:src/App.js
->>>>>>> 02048e9fea1de1757b955528ebbe2ada3e634222
         </Routes>
       </Router>
     </div>
